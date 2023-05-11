f51b5a8f5db3443ab19b1e82778d77c0e285a14f
Sergio <gigighisler@gmail.com>
2023-05-11 20:17:05 +0200

    feat(express):Finish class exercise

--- express2/ejercio_completo_clase/index.js
+++ express2/ejercio_completo_clase/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
 
 require('./database');
 
@@ -47,7 +50,32 @@ app.delete('/tareas/:id', async (req, res) => {
     }
 });
 
+app.put('/tareas/:id', async (req, res) => {
+    try{
+        const tarea = await Tarea.findByIdAndUpdate(req.params.id, req.body);
+        if (!tarea) {
+            return res.status(404).send({error: 'Tarea no encontrada'});
+        }
 
+        res.send(tarea);
+    }catch(error){
+        res.status(500).send({error: error.message});
+    }
+});
+
+app.get('/tarea_por_estado', async (req, res) => {
+    try {
+        const filtroEstado = req.query.estado || req.cookies.ultimoEstado || null;
+        const filtro = filtroEstado ? {status: filtroEstado} : {};
+        if (filtroEstado) {
+            res.cookie('ultimoEstado', filtroEstado);
+        }
+        const tareas = await Tarea.find(filtro);
+        res.send(tareas);
+    } catch(error) {
+        res.status(500).send({error: error.message});
+    }
+    });
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 }
