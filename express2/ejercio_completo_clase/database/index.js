596443f0ffaf4799eed5b5df8c6fc82a9cc8122b
Sergio <gigighisler@gmail.com>
2023-05-18 15:42:45 +0200

    feat(split): split content in diferent folders python/js

--- express2/ejercio_completo_clase/database/index.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/tareas')
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.log('Error connecting to MongoDB', err));
